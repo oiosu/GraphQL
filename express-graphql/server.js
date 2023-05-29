14b65c73d856230f8211f2645c13b17439702585
best.fe.su <kdlatnrud123@naver.com>
2023-05-29 15:42:44 +0900

    Post Comments Schema 작성하기

--- express-graphql/server.js
+++ express-graphql/server.js
@@ -7,12 +7,49 @@ const port = 4000;
 
 const schema = buildSchema(`
     type Query {
-        description: String
+        post: [Post],
+        comments: [Comments]
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        description: String!
+        comments: [Comments]
+    }
+
+    type Comment {
+        id: ID!
+        test: String!
+        likes: Int
     }
 `);
 
 const root = {
-    description: "hello world2"
+    posts: [
+    {
+        id: 'post1',
+        title: 'It is a first post',
+        description: 'It is a first post description',
+        comments: [{
+            id: 'comment1',
+            text: 'It is a first comment',
+            likes: 1
+        }]
+    },
+    {
+        id: 'post2',
+        title: 'It is a second post',
+        description: 'It is a second post description',
+        comments: [],
+    comments: [
+        {
+            id: 'comment1',
+            text: 'It is a first comment',
+            likes: 1
+        }
+
+    ]
 }
 
 
